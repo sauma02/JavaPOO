47086f8512b6b5fa19a59666969c5465863484db
saumaIqor <josedavids123@live.com>
2023-06-22 19:49:11 -0500

    Ejercicio JavaPOO HASTA EL 6

---
+++ Entidades/Cafetera.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EjerciciosPOO.Entidades;
+
+/**
+ *
+ * @author Admin
+ */
+public class Cafetera {
+    private Double capacidadMaxima;
+    private Double cantidadActual;
+
+    public Cafetera() {
+    }
+
+    public Cafetera(Double capacidadMaxima, Double cantidadActual) {
+        this.capacidadMaxima = capacidadMaxima;
+        this.cantidadActual = cantidadActual;
+    }
+
+    public Double getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    public void setCapacidadMaxima(Double capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+    }
+
+    public Double getCantidadActual() {
+        return cantidadActual;
+    }
+
+    public void setCantidadActual(Double cantidadActual) {
+        this.cantidadActual = cantidadActual;
+    }
+    
+    
+}
