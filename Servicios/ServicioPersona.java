76d5639baafec31a7819dbfc8fb467610304c367
saumaIqor <josedavids123@live.com>
2023-06-27 17:39:43 -0500

    Persona

--- Servicios/ServicioPersona.java
+++ Servicios/ServicioPersona.java
@@ -4,10 +4,27 @@
  */
 package EjerciciosPOO.Servicios;
 
+import EjerciciosPOO.Entidades.Persona;
+import java.util.Scanner;
+
 /**
  *
  * @author Admin
  */
 public class ServicioPersona {
-    
+    Scanner leer = new Scanner(System.in);
+    Persona p1 = new Persona();
+    public void crearPersona(Persona p1){
+        System.out.println("Inserte el nombre");
+        p1.setNombre(leer.nextLine());
+        System.out.println("Inserte la edad");
+        p1.setEdad(leer.nextInt());
+        System.out.println("Inserte el sexo como H, M, O");
+        p1.setGenero(leer.nextLine().charAt(0));
+        System.out.println("Ingrese el peso en kg");
+        p1.setPeso(leer.nextDouble());
+        System.out.println("Ingrese la altura en metros");
+        p1.setAltura(leer.nextDouble());
+        
+    }
 }
